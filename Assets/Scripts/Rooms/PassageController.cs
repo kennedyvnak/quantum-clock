b5cb89992b4984ad6bba4ec4317fcdca3a95a1b1
Kennedy Vinicius <kennedyvnak@gmail.com>
2024-03-13 15:55:56 -0300

    Fix quantum issues (reworked universe programming)

--- Assets/Scripts/Rooms/PassageController.cs
+++ Assets/Scripts/Rooms/PassageController.cs
@@ -42,7 +42,7 @@ namespace QuantumClock {
                 return;
             }
 
-            if (quantumEnabled && !isBlocking) 
+            if (quantumEnabled && !isBlocking && !m_TransitTo.isObserved) 
                 Transit();
         }
 
